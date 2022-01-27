f4acdf406bf6f49c73f2264bb6fb93605b47284e
Himanshu kumar <49545079+hiicoder@users.noreply.github.com>
2022-01-27 13:00:17 +0530

    Create factorial.js

---
+++ Java Script code/factorial.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+let solution = 1;
+var fact = 12;
+for(var i = fact; i > 0 ;i--)
+{
+    solution *= i;
+}
+console.log(solution);
