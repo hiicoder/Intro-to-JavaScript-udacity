998b5179ed5c445d04cd3a22410ba663ce2306fd
Himanshu kumar <49545079+hiicoder@users.noreply.github.com>
2022-01-28 22:56:50 +0530

    Update NestedNumbers.js

--- Java Script code/NestedNumbers.js
+++ Java Script code/NestedNumbers.js
@@ -1,37 +1,36 @@
 /*
- * Programming Quiz: I Got Bills (6-9)
- *
- * Use the .map() method to take the bills array below and create a second array
- * of numbers called totals. The totals array should contains each amount from the
- * bills array but with a 15% tip added. Log the totals array to the console.
- *
- * For example, the first two entries in the totals array would be:
- *
- * [57.76, 21.99, ... ]
- *
- * Things to note:
- *  - each entry in the totals array must be a number
- *  - each number must have an accuracy of two decimal points
- */
- 
- /*
+ * Programming Quiz: Nested Numbers (6-10)
  * QUIZ REQUIREMENTS
- * - Your code should have the variables `bills` and `totals`
- * - Your `bills` array should call the `map()` method and store the return of `map()` in the `totals` array
- * - Your `totals` array should be an array of numbers
- * - Your code should print the `totals` array to the console
- * - The output must be as described above. 
+ *   - The `numbers` variable is an array of arrays.
+ *   - Use a nested `for` loop to cycle through `numbers`.
+ *   - Convert each even number to the string "even"
+ *   - Convert each odd number to the string "odd"
  */
 
-var bills = [50.23, 19.12, 34.01,
-    100.11, 12.15, 9.90, 29.11, 12.99,
-    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+
+var numbers = [
+    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
 ];
-var totals = bills.map(function(tips)
+
+// your code goes here
+for(var i = 0;i<numbers.length;i++)
 {
-   tips *=1.15;
-   tips = tips.toFixed(2);
-   tips = Number(tips);
-   return tips;
-});
-    console.log(totals);
+    for(var j = 0; j<numbers.length;j++){
+        if(numbers[i][j]% 2 === 0)
+        {
+            numbers[i][j] = "even";
+        }else{
+            numbers[i][j] = "odd";
+        }
+    }
+}
+console.log(numbers);
